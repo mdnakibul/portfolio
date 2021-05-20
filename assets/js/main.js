9e5e40c7daa1c78db6666f726567a0d012e8a2b8
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-05-20 11:58:52 -0600

    contact form fixed blog added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
   $('.skillbar').skillBars({
-      from: 0,
-      speed: 4000, 
-      interval: 100,
-      decimals: 0,
+    from: 0,
+    speed: 4000,
+    interval: 100,
+    decimals: 0,
   });
-  
+
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw92b-1es8mpfnXHmfgPe5wqz-aRrBf0b5hd7z3WIn50To74P9ArQWfTd4UNi-zw4RyxA/exec'
+const form = document.forms['portfolio-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log('Hit Submit');
+  document.getElementById('timestamp').value = new Date();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
